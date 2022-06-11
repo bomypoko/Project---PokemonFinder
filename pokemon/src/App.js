3dbcb95d7d22567025ff826cfbdccef3da1f7b7c
bomypoko <orc_hero@hotmail.com>
2022-06-11 11:44:06 +0700

    If Else add! by using operater

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -54,20 +54,34 @@ export default class App extends Component {
 
     return (
 
-      <form onSubmit={this.handlerSummit}>
+      <> 
 
-      <h1>Pokemon Finder Application</h1>
+      <div className='main-div'> 
+
+      <form onSubmit={this.handlerSummit}>
+      <h3>Pokemon Finder Application</h3>
       <input type="text" value={this.state.query} onChange={this.handlerChange} />
       <input type="submit" value="Search"/>
-      <br />
-      <img src= {`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${this.state.pokemon.id}.png`} className='pokepic' alt=""/>
-      <br/>
-      <h2 className='pokename'> {this.state.pokemon.name} </h2>
-      
       </form>
+
+      {this.state.pokemon && !this.state.err ? (
+
+        <div className='pokemon-pic'> 
+            <img src= {`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${this.state.pokemon.id}.png`} className='pokepic' alt=""/>
+            <h2 className='pokemon-name-title'> {this.state.pokemon.name} </h2>
+        </div>
+
+      ) : (
+
+            <div className='error'> 
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Nuvola_apps_error.svg/1024px-Nuvola_apps_error.svg.png" className='pokeerr' alt=""/>
+              <h2> Woops! could't find that pokemon </h2>
+            </div>
+
+      )}
+      </div>
+      </>
     )
   }
-
-
 }
 
